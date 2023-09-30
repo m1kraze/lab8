7ab3ceacfb116809b5f8671ddb387e8ed8a8034b
local user <user@mil8a-118b-12.mmcs.sfedu.ru>
2023-09-30 08:48:34 +0300

    Изменил код

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
 ﻿begin
-  println('Привет, мир!');
+  var hour := ReadInteger('hour: ');
+  assert((hour >=0)and(hour <=23));
+  case hour of
+    4..10 : println('Доброе утро, мир!');
+    11..16 : println('Добрый день, мир!');
+    17..22 : println('Доброй вечер, мир!');
+    else println('Доброй ночи, мир!');
+    
+    
+  end;
+  
 end.
