cca70328167c01979f8083776c8e7c6a2dd6d83a
local user <user@mil8a-118b-12.mmcs.sfedu.ru>
2023-09-30 08:53:13 +0300

    Добавил time.pas

---
+++ time.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var a := ReadInteger('ГОД: ');
+  if (a mod 4 = 0) and (a mod 100 <> 0) or (a mod 400 = 0) then 
+    println('Високосный год') 
+    else writeln('Не високосный год');
+end.
