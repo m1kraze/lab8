0d170a9a31b66a1a110d53a90030e2f580baf5f4
local user <user@mil8a-118b-12.mmcs.sfedu.ru>
2023-09-30 09:07:05 +0300

    Программа выводит из часов,минут и секунд суммарное количество секунд

---
+++ timer.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var hours := ReadInteger('Сколько часов: ');
+  assert(hours >=0);
+  var minuts := ReadInteger('Сколько минут: ');
+  assert(minuts >=0);
+  var sec := ReadInteger('Сколько секунд: ');
+  assert(sec >=0);
+  var sum := (hours * 60 * 60) + (minuts * 60) + sec;
+  println('Суммарное количество секунд: ',sum);
+end.
