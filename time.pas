310882673a0fd001ee5204b03c8da4ca607b8be7
local user <user@mil8a-118b-12.mmcs.sfedu.ru>
2023-09-30 09:00:24 +0300

    Добавил количество дней в году

--- time.pas
+++ time.pas
@@ -1,6 +1,7 @@
 ﻿begin
   var a := ReadInteger('ГОД: ');
   if (a mod 4 = 0) and (a mod 100 <> 0) or (a mod 400 = 0) then 
-    println('Високосный год') 
-    else writeln('Не високосный год');
+    println('Високосный год: 366 дней ')
+    else writeln('Не високосный год: 365 дней')
+  
 end.
